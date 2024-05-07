e5e7cb10ab6b19d2559601ca15421c982bc16ac4
fatihgunalp <fatih.gunalp@gmail.com>
2024-05-07 12:21:06 +0300

    .

---
+++ 07.05/AspNetCore_MVCModels/AspNetCore_MVCModels/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using AspNetCore_MVCModels.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCore_MVCModels.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            Product product = new Product()
+            {
+                ID = 1,
+                ProductName = "Chai",
+                UnitPrice = 18
+            };
+
+            //model transfer
+            //15 dk
+            return View();
+        }
+    }
+}
