9e5f3126db723fcaf03bd271d5c734f974eb975b
fatihgunalp <fatih.gunalp@gmail.com>
2024-05-08 11:46:25 +0300

    .

--- 08.05/AspNetCore_MVCLayout/ASpNetCore_MVCEmptyLayout/Program.cs
+++ 08.05/AspNetCore_MVCLayout/ASpNetCore_MVCEmptyLayout/Program.cs
@@ -4,7 +4,12 @@ builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
+//rotanýn aktif edilmesi. Url'de ilgili controller içerisindeki ilgili view'lara ulaþýlmasý için aktif hale getiriyoruz.
 app.UseRouting();
+
+app.UseStaticFiles(); //wwwroot klasörünün dýþarýya açýlmasýný saðlar. Bu klasör içerisinde aþaðýdaki formatlarý doðrudan barýndýrabilir.
+//css, javascript, html, txt, image, csv, exel, word, mp3, mp4
+
 
 app.UseEndpoints(endpoints =>
 {
