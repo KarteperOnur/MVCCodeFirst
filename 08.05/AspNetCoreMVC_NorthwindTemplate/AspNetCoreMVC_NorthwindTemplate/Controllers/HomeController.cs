051f085d433e5799e85ebfb57c884af6c2661f4e
fatihgunalp <fatih.gunalp@gmail.com>
2024-05-08 13:52:00 +0300

    .

---
+++ 08.05/AspNetCoreMVC_NorthwindTemplate/AspNetCoreMVC_NorthwindTemplate/Controllers/HomeController.cs
@@ -0,0 +1,72 @@
+﻿using AspNetCoreMVC_NorthwindTemplate.Models;
+using AspNetCoreMVC_NorthwindTemplate.Models.Northwind;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace AspNetCoreMVC_NorthwindTemplate.Controllers
+{
+    public class HomeController : Controller
+    {
+
+
+        NorthwindContext _db = new NorthwindContext();
+
+
+
+
+
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var products = _db.Products.ToList();
+            return View(products);
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ 08.05/AspNetCoreMVC_NorthwindTemplate/AspNetCoreMVC_NorthwindTemplate/Controllers/ProductController.cs
@@ -0,0 +1,31 @@
+﻿using AspNetCoreMVC_NorthwindTemplate.Models.Northwind;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreMVC_NorthwindTemplate.Controllers
+{
+    public class ProductController : Controller
+    {
+        NorthwindContext _db=new NorthwindContext();
+        public IActionResult Detail(int id)//11111589
+        {
+            if (id >0)
+            {
+                var product = _db.Products.Find(id);
+                if (product != null)
+                {
+
+                    return View(product);
+                }
+                else
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            
+        }
+    }
+}
