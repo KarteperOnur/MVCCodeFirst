f81473e6f226ed3d454cea46c0c7bda5763b4872
fatihgunalp <fatih.gunalp@gmail.com>
2024-05-09 11:29:31 +0300

    data transfer samples

--- 09.05/AspNetCore_MVC_DataTransfer/AspNetCore_MVC_DataTransfer/Controllers/PersonController.cs
+++ 09.05/AspNetCore_MVC_DataTransfer/AspNetCore_MVC_DataTransfer/Controllers/PersonController.cs
@@ -5,7 +5,6 @@ namespace AspNetCore_MVC_DataTransfer.Controllers
 {
     public class PersonController : Controller
     {
-
         List<Person> _people = new List<Person>()
         {
             new Person{Firstname="John",Lastname="Doe"},
@@ -17,14 +16,22 @@ namespace AspNetCore_MVC_DataTransfer.Controllers
             new Category{CategoryName="Yiyecek"},
             new Category{CategoryName="İçecek"},
             new Category{CategoryName="Kozmetik"}
-
         };
-
 
         public IActionResult Index()
         {
+            //Title
+            ViewData["Title"] = "Samples";
+
+
             //ViewBag
+            ViewBag.People=_people;
+
+
             //ViewData
+            ViewData["Categories"] = _categories;
+
+
             return View();
         }
     }
