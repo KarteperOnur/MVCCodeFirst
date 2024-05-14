31fb26fea5218c07d6e4b23953eb39977cd215e2
Kronos <Kronos@DESKTOP-9MDFIEQ>
2024-05-14 23:01:37 +0300

    dashboard hazırlık

--- 14.05/MVC_CodeFirst/MVC_CodeFirst/Areas/Dashboard/Controllers/HomeController.cs
+++ 14.05/MVC_CodeFirst/MVC_CodeFirst/Areas/Dashboard/Controllers/HomeController.cs
@@ -1,13 +1,39 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MVC_CodeFirst.Areas.Dashboard.Models;
 
 namespace MVC_CodeFirst.Areas.Dashboard.Controllers
 {
     [Area("Dashboard")]
     public class HomeController : Controller
     {
+        //private readonly IMovieService _movieService;
+        //public HomeController(IMovieService movieService)
+        //{
+        //    _movieService = movieService;
+        //}
         public IActionResult Index()
         {
-            return View();
+            //Service datasından gelecek!
+
+            var viewModelList = new List<MovieDashboardViewModel>();
+
+            for (int i = 1; i < 50; i++)
+            {
+                var movie = new MovieDashboardViewModel();
+
+                movie.Id = i;
+                movie.Name = $"Film {i}";
+
+                movie.Description = $"Açıklama {i}";
+
+                viewModelList.Add(movie);
+            }
+
+            var result = viewModelList.OrderByDescending(p => p.Id).Take(4).ToList();
+
+            //var result = _movieService.GetDashboardMovies();
+
+            return View(result);
         }
     }
 }
---
+++ 14.05/MVC_CodeFirst/MVC_CodeFirst/Areas/Dashboard/Models/MovieDashboardViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MVC_CodeFirst.Areas.Dashboard.Models
+{
+    public class MovieDashboardViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
